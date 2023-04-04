614c28e0a21baef6f4ea4c5dc6c129a0d74c2cbe
Ole-Jakob <ole.jakob.olsen@gmail.com>
2023-04-04 22:32:01 +0200

    Loops

---
+++ loops/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // LOOP
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    // WHILE
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // FOR
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
